352f7833529501de588e4fb6066a8b39f751ba9f
Kriss Stockhaus <kriss.stockhaus@gmail.com>
2024-04-08 21:13:29 +0200

    sleep assignment

--- sleep.php
+++ sleep.php
@@ -1,1 +1,40 @@
-//my first own php code
+<?php
+include './sleepForm.html';
+class SleepAssignment
+{
+    const HOURSPERDAY = 24;
+    public $userFirstName;
+    public $userAge;
+    public $userHoursSlept;
+
+    public function __construct($cFirstname, $cAge, $cHoursSlept)
+    {
+        $this->userFirstName = $cFirstname;
+        $this->userAge = $cAge;
+        $this->userHoursSlept = $cHoursSlept;
+    }
+
+    public function calcSleptYears()
+    {
+        $sleptYears = ($this->userHoursSlept / self::HOURSPERDAY * $this->userAge);
+        return $sleptYears;
+    }
+
+    public function displayMessage()
+    {
+        $sleptYears = $this->calcSleptYears();
+        print "<p>Hey $this->userFirstName you have slept $sleptYears !";
+    }
+}
+
+if (!isset($_POST['userfirstname'])) {//skip
+} else {
+    $postFirstName = $_POST['userfirstname'];
+    $postUserHours = $_POST['userhours'];
+    $postUserAge = $_POST['userage'];
+
+    $mySleepAssignment = new SleepAssignment($postFirstName, $postUserAge, $postUserHours);
+
+    $mySleepAssignment->displayMessage();
+}
+
